3a79a1f08297f841a69a4302019c34904ad169db
Extractonico <extractonicotono@gmail.com>
2022-09-25 16:00:56 -0500

    Corregi en base a comentarios del PR

--- src/java/ReadFileUtil.java
+++ src/java/ReadFileUtil.java
@@ -2,12 +2,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 public class ReadFileUtil {
-    public ReadFileUtil() {
-    }
 
+    private ReadFileUtil(){};
     public static String readFile(String path) {
         String contenido = "";
         StringBuilder s1 = new StringBuilder("");
